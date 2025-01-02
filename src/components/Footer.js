bbfa3bf260b39122970b9cb4c70339ed5b7269ac
Kevin Leonardi <kevinleonardi@yahoo.com>
2025-01-03 05:05:25 +0700

    first commit X x

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Instagram,Twitter, Facebook, LinkedIn} from '@mui/icons-material'
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='socialMedia'>
+        <Instagram/> <Twitter /><Facebook/><LinkedIn/>
+      </div>
+      <p>&copy; 2021 pedrotechpizza.com</p>
+    </div>
+  )
+}
+
+export default Footer
